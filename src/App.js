579ed1a7797f66d4dfefde23611d9e0c33aacdb7
Seshan Ravikumar <seshan10@me.com>
2019-07-15 16:02:12 -0400

    Many Changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Pages
+import Home from "./Pages/Home";
+import Posts from "./Pages/Posts";
+
+// Assets
+import Background from './Assets/bg.jpg';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Load the Bootstrap CSS
+
+class App extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Router>
+                    <div style={RootStyle}>
+                        <nav style={NavStyle}>
+                            <div style={NavButtonStyle}>Seshan Ravikumar</div>
+                            <Link to="/" style={NavButtonStyle}>Home</Link>
+                            <Link to="/posts/" style={NavButtonStyle}>Posts</Link>
+                        </nav>
+                        <br />
+                        <Switch>
+                            <Route path="/" exact component={Home} />
+                            <Route path="/posts/" component={Posts} />
+                        </Switch>
+                        <br />
+                        <nav style={NavStyle}>
+                            <div style={NavButtonStyle}>(c) 2019 Seshan Ravikumar.</div>
+                            <div style={NavButtonStyle}>This website is Free Software under the GNU AGPL 3.0.</div>
+                        </nav>
+                    </div>
+                </Router>
+            </React.Fragment>
+        );
+    }
 }
+const RootStyle= {
+    height: '100%',
+};
+const NavStyle = {
+    display: 'flex',
+    backgroundImage: `url(${Background})`
+};
+const NavButtonStyle = {
+    padding: '1em',
+    color: 'white',
+    borderRightStyle: 'solid'
+};
 
 export default App;
---
+++ src/Pages/Home.js
@@ -0,0 +1,58 @@
+import React, {Component} from "react";
+import { Container, Jumbotron, Button } from 'react-bootstrap';
+import axios from 'axios';
+import { Circle } from 'react-preloaders';
+import {Helmet} from 'react-helmet';
+
+class Home extends Component {
+    // eslint-disable-next-line no-useless-constructor
+    constructor(props) {
+        super(props);
+        this.state = {
+            contentLoaded: false,
+            homeContent: 'Loading...'
+        };
+
+        // Call the WP REST API and populate content.
+        this.loadDataFromWP().then((data) => {
+            console.log(data);
+            setTimeout(() => {
+                this.setState({
+                    contentLoaded: true,
+                    homeContent: data
+                });
+            }, 0);
+        });
+    }
+
+    async loadDataFromWP() {
+        try {
+            const response = await axios.get('https://seshan.xyz/wp-json/wp/v2/pages/2');
+            //console.log(response.data.content.rendered);
+            return response.data.content.rendered;
+        } catch (error) {
+            console.error(error);
+            return "Error Loading Data";
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <Helmet>
+                    <title>Seshan's Personal Website - Home</title>
+                </Helmet>
+                <Container>
+                    <Jumbotron>
+                        <h1>Hello, world!</h1>
+                        <div dangerouslySetInnerHTML={{__html: this.state.homeContent}} />
+                    </Jumbotron>
+                </Container>
+                <Circle time={100} customLoading={!this.state.contentLoaded} />
+            </div>
+        );
+    }
+}
+
+
+export default Home;
